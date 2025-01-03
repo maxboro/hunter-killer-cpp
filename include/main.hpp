8176567c8c662e462fd7c1ac2698866aa62b43f6
Max B. <maksym_borovyk@ukr.net>
2025-01-03 18:29:19 +0200

    Remove placeholder code

--- include/main.hpp
+++ include/main.hpp
@@ -1,7 +1,6 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-#include "test.hpp"
 #include "Settings.hpp"
 
 int main(){
@@ -9,7 +8,6 @@ int main(){
     Settings* settings = Settings::get_instance();
     settings->load_settings();
 
-    std::cout << foo() << std::endl;
     std::cout << "n_prey: " << settings->get_value("n_prey") << std::endl;
     return 0;
 }
--- unit_tests/test_main.cpp
+++ unit_tests/test_main.cpp
@@ -1,12 +1,2 @@
 #define CATCH_CONFIG_MAIN
 #include "../external/catch2/catch_amalgamated.hpp"
-#include "../include/test.hpp"
-
-
-TEST_CASE("Example test", "[example]") {
-    REQUIRE(1 + 1 == 2);
-}
-
-TEST_CASE("Example test foo from test.hpp", "[example_foo_test.hpp]") {
-    REQUIRE(foo() == "Test");
-}
