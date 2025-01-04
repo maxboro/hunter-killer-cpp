cdab405735a4da9327732b46b4857e3c94beed28
Max B. <77401015+maxboro@users.noreply.github.com>
2025-01-04 10:19:57 +0200

    Add Location class (#5)
    * Add Location
    
    * Refactor: make x,y private, add != overloading
    
    * Add tests for refactored code

---
+++ include/location.hpp
@@ -0,0 +1,42 @@
+#ifndef LOCATION_HPP
+#define LOCATION_HPP
+
+#include <sstream>
+#include <ostream>
+
+class Location{
+public:
+    Location(int x, int y): _x(x), _y(y) {};
+
+    int get_x() const {return _x;}
+    int get_y() const {return _y;}
+
+    // Overloading ==
+    bool operator==(const Location& other) const {
+        return _x == other.get_x() && _y == other.get_y();
+    }
+
+    // Overloading !=
+    bool operator!=(const Location& other) const {
+        return !(*this == other);
+    }
+
+    // Overload <<
+    friend std::ostream& operator<<(std::ostream& os, const Location& obj) {
+        os << "Loc[" << obj._x << "; " << obj._y << "]";
+        return os;
+    }
+
+    // string representation
+    std::string to_str() const {
+        std::ostringstream oss;
+        oss << *this;
+        return oss.str();
+    }
+
+private:
+    int _x;
+    int _y;
+};
+
+#endif
