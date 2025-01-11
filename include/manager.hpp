9e10a9e314ad70a244d7cff17fffd5a70800d2fe
Max B. <maksym_borovyk@ukr.net>
2025-01-11 14:46:44 +0200

    Add manager

---
+++ include/manager.hpp
@@ -0,0 +1,36 @@
+#ifndef MANAGER_HPP
+#define MANAGER_HPP
+
+#include "game.hpp"
+
+class Manager{
+public:
+    Manager(Game game, Settings* settings): 
+        _game(game),  
+        _settings(settings){};
+
+
+    void _add_prey(){
+        const int n_prey = _settings->get_value("n_prey");
+        for (int prey_id = 0; prey_id < n_prey; prey_id++){
+            std::string prey_name = "Prey_" + std::to_string(prey_id);
+            _game.add_prey(prey_name);
+        }
+    }
+    void run_game(){
+        _add_prey();
+
+        // game loop
+        const int n_steps = _settings->get_value("n_steps");
+        for (int i = 0; i < n_steps; i++){
+            _game.make_move();
+            _game.show_status();
+        }
+    }
+
+private:
+    Game _game;
+    Settings* _settings;
+};
+
+#endif
